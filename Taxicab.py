76937ca65c0b2b471ad83d4ff2a3a270c1c40381
sophiacphilips <114261752+sophiacphilips@users.noreply.github.com>
2022-10-26 20:38:39 -0700

    commit 3 updated file name, made odometer private, commented test codes

--- Taxicab.py
+++ Taxicab.py
@@ -47,9 +47,9 @@ class Taxicab:
         self._y += distance
         self._odometer += abs(distance)
 
-cab=Taxicab(5,-8) #creates a Taxicab object at coordinates (5, -8)
-cab.move_x(3) # moves cab 3 units "right"
-cab.move_y(-4) # moves cab 4 units "down"
-cab.move_x(-1) #moves cab 1 unit "down"
-print(cab.get_odometer()) #prints the current odometer reading
+#cab=Taxicab(5,-8) #creates a Taxicab object at coordinates (5, -8)
+#cab.move_x(3) # moves cab 3 units "right"
+#cab.move_y(-4) # moves cab 4 units "down"
+#cab.move_x(-1) #moves cab 1 unit "down"
+#print(cab.get_odometer()) #prints the current odometer reading
 # At this point the cab has traveled 3 + 4 + 1 = 8 units and is now at coordinates (7, -12)
